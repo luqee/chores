874850f26e9d6d024a0d04da779503d6c44c0249
Luke Nzang'u <saintlukke@gmail.com>
2017-07-16 23:44:59 +0300

    Set up providers location map view

---
+++ app/src/main/java/com/ntatma/tatua/FragmentProviders.java
@@ -0,0 +1,13 @@
+package com.ntatma.tatua;
+
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * Created by luqi on 7/16/17.
+ */
+
+public class FragmentProviders extends SupportMapFragment{
+    private GoogleMap googleMap;
+}
